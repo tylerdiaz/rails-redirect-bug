9e8ef7b195a9b3177c16e592710df3a95518b4bd
Tyler J. Diaz <me@tylerdiaz.com>
2017-02-16 13:57:47 +0100

    add bug reproduction

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,7 +1,13 @@
 class TestController < ApplicationController
   def index
+    redirect_to test_destination_path
+  end
+
+  def destination
+    render text: "Hello"
   end
 
   def location
+    raise 'Error for some reason'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'test/index'
-
+  get 'test/destination'
   get 'test/location'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
